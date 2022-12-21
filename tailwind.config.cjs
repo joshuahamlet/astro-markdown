694a2f0b54f9fa39fc72c4d430c45ac510156fac
Joshua Hamlet <jhamlet@tuta.io>
2022-12-20 16:05:20 -0800

    feat: tailwind config and refactoring

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-    backgroundImage: {
-      'gradient-accent': 'linear-gradient(45deg, rgb(var(--accent)), #da62c4 30%, white 60%)'
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        accent: '124, 58, 237',
+        accentGradient:
+          'linear-gradient(45deg, rgb(var(--accent)), #da62c4 30%, white 60%)',
+        dracBackground: '#282a36',
+        dracCurrentLine: '#44475a',
+        dracForeground: '#f8f8f2',
+        dracComment: '#6272a4',
+        dracCyan: '#8be9fd',
+        dracGreen: '#50fa7b',
+        dracOrange: '#ffb86c',
+        dracPink: '#ff79c6',
+        dracPurple: '#bd93f9',
+        dracRed: '#ff5555',
+        dracYellow: '#f1fa8c',
+      },
+      backgroundImage: {
+        'gradient-accent':
+          'linear-gradient(45deg, rgb(var(--accent)), #da62c4 30%, white 60%)',
+      },
+      backgroundSize: {
+        "400%": "400%"
+      }
     },
-		extend: {},
-	},
-	plugins: [],
+  },
+  plugins: [],
 }
